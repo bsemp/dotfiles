615b5ce8aa8070e9fe1d433fdce26cb3ffba0d06
Bruno SEMPERLOTTI <bruno.semperlotti@gmail.com>
2020-01-21 18:10:52 +0100

    update python virtualenv segment
    Enable python version display

--- roles/zsh/files/.p10k.zsh
+++ roles/zsh/files/.p10k.zsh
@@ -568,7 +568,7 @@
   # typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=0
   # typeset -g POWERLEVEL9K_VIRTUALENV_BACKGROUND=4
   # Don't show Python version next to the virtual environment name.
-  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
+  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=true
   # Separate environment name from Python version only with a space.
   typeset -g POWERLEVEL9K_VIRTUALENV_{LEFT,RIGHT}_DELIMITER=
   # Custom icon.
