55aad25446781e235ad659e6be3ead651fd415be
Bruno Semperlotti <bruno.semperlotti@gmail.com>
2021-04-30 11:17:13 +0200

    Update ls aliases if exa is installed
    EXA is a modern replacement for ls
    https://the.exa.website/

--- roles/zsh/files/custom/system.zsh
+++ roles/zsh/files/custom/system.zsh
@@ -13,4 +13,19 @@ alias antigenconfig="${EDITOR:-vim} ${ZDOTDIR}/.antigenrc"
 alias p10kconfig="${EDITOR:-vim} ${ZDOTDIR}/.p10k.zsh"
 
 # System
-alias ll="ls -lAhp"
+if command -v exa &>/dev/null; then
+  # Override common ls aliases whith exa (https://the.exa.website/) if available
+  alias ls='exa'
+  alias l='exa -lbF --git'
+  alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'
+  alias ll='exa -lah'
+  alias lt='exa --tree --level=2'
+else
+  # Override common ls aliases
+  alias ls='ls -G'
+  alias l='ls -lFh'
+  alias la='ls -lAFhp'
+  alias ll='ls -lAhp'
+  alias lr='ls -tRFh'
+  alias lt='ls -ltFh'
+fi
