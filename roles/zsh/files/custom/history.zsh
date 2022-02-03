fbc212b3f97f73a9ac01058becc93c093f97efbb
Bruno Semperlotti <bruno.semperlotti@gmail.com>
2022-02-03 10:07:36 +0100

    fix(zsh): fix history cmd configuration

---
+++ roles/zsh/files/custom/history.zsh
@@ -0,0 +1,22 @@
+
+# Configure history
+export HISTFILE=${ZDOTDIR}/.zsh_history
+export HISTSIZE=50000
+export SAVEHIST=${HISTSIZE}
+# /!\ Needs "setopt extendedglob"
+# "#" means optional. Ex: "vim#( *)#" matches "vi", "vim", "vim .*", "vi .* "
+export HISTORY_IGNORE='([bf]g *|l[alsh]#( *)#|history|cd( *)#|z( *)#|git add *|git reset *|pwd|exit|date|*--help|vim#( *)#|vault*)'
+
+# https://zsh.sourceforge.io/Doc/Release/Options.html#History
+setopt HIST_IGNORE_SPACE
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_NO_STORE
+
+# https://zsh.sourceforge.io/Doc/Release/Functions.html#Hook-Functions
+# https://zsh.sourceforge.io/Doc/Release/Parameters.html#Parameters-Used-By-The-Shell
+zshaddhistory() {
+  emulate -L zsh
+  # HISTORY_IGNORE should use EXTENDED_GLOB syntax
+  setopt extendedglob
+  [[ $1 != ${~HISTORY_IGNORE} ]]
+}
