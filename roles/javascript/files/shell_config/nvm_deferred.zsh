7de869e9fa9557ecee35756fff7117d3108bf286
Bruno Semperlotti <bruno.semperlotti@gmail.com>
2023-06-14 19:00:43 +0200

    chore: add flag --no-use to nvm startup script
    The flag is supposed to delay nvm initialization until nvm use is called, gaining some time during shell startup

--- roles/javascript/files/shell_config/nvm_deferred.zsh
+++ roles/javascript/files/shell_config/nvm_deferred.zsh
@@ -1,5 +1,5 @@
 export NVM_DIR="$(brew --prefix)/opt/nvm"
 # Load nvm
-[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && source "$(brew --prefix)/opt/nvm/nvm.sh"
+[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && source "$(brew --prefix)/opt/nvm/nvm.sh" --no-use
 # Load nvm bash_completion
 [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && source "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"
