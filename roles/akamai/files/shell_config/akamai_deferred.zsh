aa1406922b1d485f22e2ece49700e3e7fe07a96d
Bruno Semperlotti <bruno.semperlotti@gmail.com>
2023-06-14 18:58:03 +0200

    fix: make akamai docker image version explicit

--- roles/akamai/files/shell_config/akamai_deferred.zsh
+++ roles/akamai/files/shell_config/akamai_deferred.zsh
@@ -1,7 +1,7 @@
 # https://github.com/akamai/akamai-docker
 # https://github.com/akamai/cli#install-with-docker
-alias akamai-shell='docker run -it --rm -v ${XDG_CONFIG_HOME}/akamai/.edgerc:/root/.edgerc:ro akamai/shell'
-alias akamai='docker run -it --rm -v ${XDG_CONFIG_HOME}/akamai/.edgerc:/root/.edgerc:ro akamai/shell akamai'
+alias akamai-shell='docker run -it --rm -v ${XDG_CONFIG_HOME}/akamai/.edgerc:/root/.edgerc:ro akamai/shell:latest'
+alias akamai='docker run -it --rm -v ${XDG_CONFIG_HOME}/akamai/.edgerc:/root/.edgerc:ro akamai/shell:latest akamai'
 
 # Useful functions
 export AKAMAI_PRAGMA_HEADERS_FULL="akamai-x-get-client-ip, akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no, akamai-x-feo-trace, akamai-x-get-request-id, akamai-x-get-cache-tags"
