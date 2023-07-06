6c70a429bc6afaef7e1bef6f765eca874866549d
Bruno Semperlotti <bruno.semperlotti@gmail.com>
2023-07-06 15:12:08 +0200

    fix: use fully qualified docker images

--- roles/aws/files/shell_config/aws_deferred.zsh
+++ roles/aws/files/shell_config/aws_deferred.zsh
@@ -1,3 +1,3 @@
 
 # https://hub.docker.com/r/amazon/aws-cli
-alias aws="docker run --rm -ti -v ${XDG_CONFIG_HOME}/aws:/root/.aws -v $(pwd):/aws amazon/aws-cli:latest"
+alias aws="docker run --rm -ti -v ${XDG_CONFIG_HOME}/aws:/root/.aws -v $(pwd):/aws docker.io/amazon/aws-cli:latest"
