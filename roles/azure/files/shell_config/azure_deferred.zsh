2780537bc3a9117da4944deb4dca35a9dd9a75d2
Bruno Semperlotti <7171722+bsemp@users.noreply.github.com>
2024-04-18 15:34:22 +0200

    fix: azure directory symlink
    Starship prompt expect azure directory to be at user HOME

--- roles/azure/files/shell_config/azure_deferred.zsh
+++ roles/azure/files/shell_config/azure_deferred.zsh
@@ -1,2 +1,2 @@
 # https://learn.microsoft.com/en-us/cli/azure/run-azure-cli-docker?view=azure-cli-latest
-alias az="docker run --rm -ti -v ${XDG_CONFIG_HOME}/azure:/root/.azure mcr.microsoft.com/azure-cli:latest az
+alias az="docker run --rm -ti -v ${XDG_CONFIG_HOME}/azure:/root/.azure mcr.microsoft.com/azure-cli:latest az"
