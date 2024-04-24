57e728fc4f8794dd69f4479d5b5b9097de4cc880
Bruno Semperlotti <7171722+bsemp@users.noreply.github.com>
2024-04-24 11:33:56 +0200

    fix(gcloud): update docker image tag for ARM64 compatibility

--- roles/gcloud/files/shell_config/gcloud_deferred.zsh
+++ roles/gcloud/files/shell_config/gcloud_deferred.zsh
@@ -1,2 +1,2 @@
-
-alias gcloud="docker run -ti --rm -v ${XDG_CONFIG_HOME}/gcloud:/root/.config/gcloud gcr.io/google.com/cloudsdktool/google-cloud-cli:latest gcloud"
+# Use ARM64 compatible image (https://cloud.google.com/sdk/docs/downloads-docker)
+alias gcloud="docker run -ti --rm -v ${XDG_CONFIG_HOME}/gcloud:/root/.config/gcloud gcr.io/google.com/cloudsdktool/google-cloud-cli:debian_component_based gcloud"
