d3d0529b34cc4879a729672521b41584a976da31
Bruno Semperlotti <7171722+bsemp@users.noreply.github.com>
2025-02-04 16:57:08 +0100

    feat: install ghostty and tweak zellij accordingly

--- roles/cli_tools/files/shell_config/zellij.zsh
+++ roles/cli_tools/files/shell_config/zellij.zsh
@@ -1,8 +1,8 @@
 ##########
 ## zellij, terminal multiplexer
 ##########
-# Autostart zellij only in alacritty terminal (prevent zellij from starting in vscode integrated terminal)
-if [[ "${TERM}" == "alacritty" ]]; then
+# Autostart zellij but prevent starting in vscode integrated terminal
+if [[ "${TERM_PROGRAM}" != "vscode" ]]; then
   if command -v zellij &>/dev/null; then
     export ZELLIJ_AUTO_ATTACH="true"
     eval "$(zellij setup --generate-auto-start zsh)"
