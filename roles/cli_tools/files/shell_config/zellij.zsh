be61a24d6240754355e696597fe3b4f810c45d6b
Bruno Semperlotti <7171722+bsemp@users.noreply.github.com>
2025-09-19 14:32:00 +0200

    feat: stop starting zellij automatically in every terminal

--- roles/cli_tools/files/shell_config/zellij.zsh
+++ roles/cli_tools/files/shell_config/zellij.zsh
@@ -2,9 +2,9 @@
 ## zellij, terminal multiplexer
 ##########
 # Autostart zellij but prevent starting in vscode integrated terminal
-if [[ "${TERM_PROGRAM}" != "vscode" ]]; then
-  if command -v zellij &>/dev/null; then
-    export ZELLIJ_AUTO_ATTACH="true"
-    eval "$(zellij setup --generate-auto-start zsh)"
-  fi
-fi
+# if [[ "${TERM_PROGRAM}" != "vscode" ]]; then
+#   if command -v zellij &>/dev/null; then
+#     export ZELLIJ_AUTO_ATTACH="true"
+#     eval "$(zellij setup --generate-auto-start zsh)"
+#   fi
+# fi
